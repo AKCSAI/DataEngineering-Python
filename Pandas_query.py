7858ae0143cd7a379fdc1513c090f4e19a7ac3da
Aziz Khan <manruption@gmail.com>
2025-04-16 14:01:45 -0400

    Query RDS using pandas

---
+++ Pandas_query.py
@@ -0,0 +1,14 @@
+# Import packages
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query("SELECT * FROM Employee WHERE EmployeeID >= 6 Order BY BirthDate", engine)
+
+# Print head of DataFrame
+print(df.head())
+
+
